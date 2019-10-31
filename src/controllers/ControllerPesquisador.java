b0aa30d63b7dba123c35e3c80e2ad9e9f9fef54e
Anderson Santos <andeson.santos.silva@ccc.ufcg.edu.br>
2019-10-31 11:31:17 -0300

    Caso de Uso 2 aceito nos testes de aceitacao. Documentacao da classe Pesquisador completa e do Controller em progresso. O controlador poderia ser beneficiado por melhoria do codigo.

--- src/controllers/ControllerPesquisador.java
+++ src/controllers/ControllerPesquisador.java
@@ -5,39 +5,51 @@ import com.psquiza.entidades.Pesquisador;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ *  Classe Controladora da Classe Pesquisador. Responsavel por criar, guardar e manipular os pesquisadores no sistema.
+ */
 public class ControllerPesquisador {
+    /**
+     *  Mapa armazenando pesquisadores no controlador. Pesquisadores sao identificados unicamente pelo seu email.
+     */
     private Map<String, Pesquisador> pesquisadores;
 
+    /**
+     *  Constroi um objeto ControllerPesquisador, inicializando o Mapa na forma de um HashMap
+     */
     public ControllerPesquisador() {
         pesquisadores =  new HashMap<>();
     }
 
+    /**
+     *  Cadastra um pesquisador no controle, criando um novo Pesquisador a partir de seu nome, funcao, biografia, email e endereco da foto, em seguida
+     *  armazena no Mapa pesquisadores a partir de seu email.
+     *
+     * @param nome String representando o nome de um Pesquisador.
+     * @param funcao String representando a funcao de um Pesquisador.
+     * @param biografia String representando a biografia de um Pesquisador.
+     * @param email String representando o email de um Pesquisador.
+     * @param fotoURL String representando o endereco URL da foto de um Pesquisador.
+     */
     public void cadastraPesquisador(String nome, String funcao, String biografia, String email, String fotoURL){
-        if (nome == null){
-            throw new NullPointerException("Campo nome nao pode ser nulo ou vazio.");
-        } else if (nome.equals("")){
-            throw new IllegalArgumentException("Campo nome nao pode ser nulo ou vazio.");
-        }
-        if (funcao == null){
-            throw new NullPointerException("Campo funcao nao pode ser nulo ou vazio.");
-        } else if (funcao.equals("")){
-            throw new IllegalArgumentException("Campo funcao nao pode ser nulo ou vazio.");
-        }
-        if (biografia == null){
-            throw new NullPointerException("Campo biografia nao pode ser nulo ou vazio.");
-        } else if (biografia.equals("")){
-            throw new IllegalArgumentException("Campo biografia nao pode ser nulo ou vazio.");
-        }
-        if (!verificaEmail(email)){
-            throw new IllegalArgumentException("Formato de email invalido.");
-        }
-        if (!verificaFoto(fotoURL)){
-            throw new IllegalArgumentException("Formato de foto invalido.");
-        }
+        verificaNome(nome);
+        verificaFuncao(funcao);
+        verificaBio(biografia);
+        verificaEmail(email);
+        verificaFoto(fotoURL);
         pesquisadores.put(email , new Pesquisador(nome, funcao, biografia, email, fotoURL));
     }
 
-    private boolean verificaEmail(String email){
+    /**
+     *  Verifica uma String representando um possivel email de um Pesquisador. Verifica se o email nao eh nulo ou vazioa, em seguida
+     *  se segue o formato adequado.
+     *  Formato Adequado: texto alfanumerico + @ + texto alfanumerico.
+     *  Caso o email nao esteja adequada, sao lancadas excecoes com a mensagem adequada.
+     *
+     * @param email
+     */
+    private void verificaEmail(String email){
+        boolean valido = false;
         if (email == null){
             throw new NullPointerException("Campo email nao pode ser nulo ou vazio.");
         } else if (email.equals("")){
@@ -52,13 +64,26 @@ public class ControllerPesquisador {
                     pos = i;
                 }
             }
-            if (pos + 1 < email.length()){
-                return true;
+            if (pos + 1 < email.length() && pos != 0){
+                valido = true;
             }
         }
-        return false;
+
+        if(!valido){
+            throw new IllegalArgumentException("Formato de email invalido.");
+        }
     }
-    private boolean verificaFoto(String foto){
+
+    /**
+     *  Verifica uma String representando o possivel endereco de URL da foto de um Pesquisador. Avaliando se a String nao eh
+     *  nula ou vazia e em seguida se segue o formato adequado.
+     *  Formato Adequado: https:// + restante do texto
+     *  Formato Adequado: http:// + restante do texto
+     *
+     * @param foto
+     */
+    private void verificaFoto(String foto){
+        boolean valido = false;
         if (foto == null){
             throw new NullPointerException("Campo fotoURL nao pode ser nulo ou vazio.");
         } else if (foto.equals("")){
@@ -66,13 +91,63 @@ public class ControllerPesquisador {
         }
 
         if (foto.length() >= 8){
-            return foto.substring(0, 8).equals("https://");
+            if(foto.substring(0, 8).equals("https://")){
+                valido = true;
+            }
         } else if (foto.length() >= 7){
-            return foto.substring(0, 7).equals("http://");
+            if( foto.substring(0, 7).equals("http://")){
+                valido = true;
+            }
         }
-        return false;
+        if (!valido){
+            throw new IllegalArgumentException("Formato de foto invalido.");
+        }
     }
 
+    /**
+     *  Verifica uma String representando a possivel funcao de um Pesquisador. Avalia se a String eh nula ou vazia.
+     *
+     * @param funcao
+     */
+    private void verificaFuncao (String funcao){
+        if (funcao == null){
+            throw new NullPointerException("Campo funcao nao pode ser nulo ou vazio.");
+        } else if (funcao.equals("")){
+            throw new IllegalArgumentException("Campo funcao nao pode ser nulo ou vazio.");
+        }
+    }
+    /**
+     *  Verifica uma String representando o possivel nome de um Pesquisador. Avalia se a String eh nula ou vazia.
+     *
+     * @param nome
+     */
+    private void verificaNome(String nome){
+        if (nome == null){
+            throw new NullPointerException("Campo nome nao pode ser nulo ou vazio.");
+        } else if (nome.equals("")){
+            throw new IllegalArgumentException("Campo nome nao pode ser nulo ou vazio.");
+        }
+    }
+    /**
+     *  Verifica uma String representando a possivel biografia de um Pesquisador. Avalia se a String eh nula ou vazia.
+     *
+     * @param biografia
+     */
+    private void verificaBio (String biografia){
+        if (biografia == null){
+            throw new NullPointerException("Campo biografia nao pode ser nulo ou vazio.");
+        } else if (biografia.equals("")){
+            throw new IllegalArgumentException("Campo biografia nao pode ser nulo ou vazio.");
+        }
+    }
+
+    /**
+     *  Altera um atributo de um pesquisador, identificado por seu email.
+     *
+     * @param email
+     * @param atributo
+     * @param novoValor
+     */
     public void alteraPesquisador(String email, String atributo, String novoValor){
         verificaEmail(email);
         if (atributo == null){
@@ -80,36 +155,45 @@ public class ControllerPesquisador {
         } else if (atributo.equals("")){
             throw new IllegalArgumentException("Atributo nao pode ser vazio ou nulo.");
         }
-        if (novoValor == null){
-            throw  new NullPointerException();
-        } else if(novoValor.equals("")){
-            throw new IllegalArgumentException();
-        }
         if (!pesquisadores.containsKey(email)){
-            throw new IllegalArgumentException("Pesquisador nao encontrado.");
+            throw new IllegalArgumentException("Pesquisador nao encontrado");
         }
         if (!pesquisadores.get(email).ehAtivo()){
             throw new IllegalArgumentException("Pesquisador inativo.");
         }
 
         if (atributo.equals("NOME")){
+            verificaNome(novoValor);
             pesquisadores.get(email).setNome(novoValor);
         } else if(atributo.equals("FUNCAO")){
+            verificaFuncao(novoValor);
             pesquisadores.get(email).setFuncao(novoValor);
         } else if (atributo.equals("BIOGRAFIA")){
+            verificaBio(novoValor);
             pesquisadores.get(email).setBio(novoValor);
         } else if (atributo.equals("EMAIL")){
-            if (verificaEmail(novoValor)) {
-                pesquisadores.put(novoValor, pesquisadores.get(email));
-                pesquisadores.remove(email);
-            }
+            verificaEmail(novoValor);
+            pesquisadores.put(novoValor, pesquisadores.get(email));
+            pesquisadores.remove(email);
+            pesquisadores.get(novoValor).setEmail(novoValor);
+
         } else if (atributo.equals("FOTO")){
-            if (verificaFoto(novoValor)) {
-                pesquisadores.get(email).setFoto(novoValor);
-            }
+            verificaFoto(novoValor);
+            pesquisadores.get(email).setFoto(novoValor);
+
+        } else {
+            throw new IllegalArgumentException("Atributo invalido.");
         }
     }
     public boolean pesquisadorEhAtivo(String email){
+        if(email == null){
+            throw new NullPointerException("Email nao pode ser vazio ou nulo.");
+        } else if (email.trim().equals("")){
+            throw new IllegalArgumentException("Email nao pode ser vazio ou nulo.");
+        }
+        if (!pesquisadores.containsKey(email)){
+            throw new IllegalArgumentException("Pesquisador nao encontrado");
+        }
         return pesquisadores.get(email).ehAtivo();
     }
     public void desativaPesquisador(String email){
@@ -134,6 +218,9 @@ public class ControllerPesquisador {
     }
     public String exibePesquisador(String email){
         verificaEmail(email);
+        if (!pesquisadores.containsKey(email)){
+            throw new IllegalArgumentException("Pesquisador nao encontrado");
+        }
         if (!pesquisadores.get(email).ehAtivo()){
             throw new IllegalArgumentException("Pesquisador inativo.");
         }
--- src/entidades/Pesquisador.java
+++ src/entidades/Pesquisador.java
@@ -1,13 +1,43 @@
 package com.psquiza.entidades;
 
+/**
+ *  Representa um Pesquisador no Sistema.
+ */
 public class Pesquisador {
+    /**
+     *  Representacao em String da funcao de um pesquisador.
+     */
     private String funcao;
+    /**
+     *  Representacao em String da Biografia de um pesquisador.
+     */
     private String bio;
+    /**
+     *  Representacao em String do Nome de um pesquisador.
+     */
     private String nome;
+    /**
+     *  Representacao em String do email de um pesquisador.
+     */
     private String email;
+    /**
+     *  Representacao em String do endereco URL da foto de um pesquisador."
+     */
     private String foto;
+    /**
+     *  Representacao em valor booleano da situacao de atividade de um pesquisador."
+     */
     private boolean ativo;
 
+    /**
+     *  Constroi um objeto Pesquisador a partir de informacoes de seu nome, funcao, biografia, email e foto. O pesquisador eh criado em estado ativo.
+     *
+     * @param nome String representando o nome de um(a) pesquisador(a).
+     * @param funcao String representando a funcao de um(a) pesquisador(a).
+     * @param bio String representando a biografia de um(a) pesquisador(a).
+     * @param email String representando o email de um(a) pesquisador(a).
+     * @param foto String representando o endereco URL da foto de um(a) pesquisador(a).
+     */
     public Pesquisador(String nome, String funcao, String bio, String email, String foto) {
         this.funcao = funcao;
         this.bio = bio;
@@ -17,39 +47,84 @@ public class Pesquisador {
         this.ativo = true;
     }
 
+    /**
+     *  Metodo responsavel por verificar se um Pesquisador eh ativo, retornando true em caso afirmativo e false no contrario.
+     *
+     * @return Valor Boolean representando a situacao de atividade do Pesquisador.
+     */
     public boolean ehAtivo() {
         return ativo;
     }
 
+    /**
+     *  Define um valor String para o atributo email do Pesquisador.
+     *
+     * @param email String representando o novo email a ser definido.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *  Define um valor Boolean para o atributo ativo do Pesquisador.
+     *
+     * @param ativo Boolean representando o novo valor do atributo ativo a ser definido.
+     */
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
     }
 
+    /**
+     *  Define um valor String para o atributo funcao do Pesquisador.
+     *
+     * @param funcao String representando o novo valor do atributo funcao a ser definido.
+     */
     public void setFuncao(String funcao) {
         this.funcao = funcao;
     }
 
+    /**
+     *  Define um valor String para o atributo Biografia do Pesquisador.
+     *
+     * @param bio String representando o novo valor do atributo bio a ser definido.
+     */
     public void setBio(String bio) {
         this.bio = bio;
     }
-
+    /**
+     *  Define um valor String para o atributo nome do Pesquisador.
+     *
+     * @param bio String representando o novo valor do atributo nome a ser definido.
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    /**
+     *  Define um valor String para o atributo endereco URL da foto do Pesquisador.
+     *
+     * @param bio String representando o novo valor do atributo foto a ser definido.
+     */
     public void setFoto(String foto) {
         this.foto = foto;
     }
 
+    /**
+     *  Faz a representacao do Pesquisador em forma de String, contendo seu nome, funcao, biografia, email e endereco de foto.
+     *  No formato NOME (FUNCAO) - BIOGRAFIA - EMAIL - URLFOTO
+     *
+     * @return String representando um Pesquisador.
+     */
     @java.lang.Override
     public java.lang.String toString() {
         return (nome + " (" + funcao + ")" +  " - " + bio + " - " + email + " - " + foto );
     }
 
+    /**
+     *  Avalia se dois objetos sao do tipo Pesquisador e sao iguais. Retornando verdadeiro caso possuam o mesmo email.
+     *
+     * @param object Objeto a ser comparado se eh igual.
+     * @return Valor Boolean indicando se os objetos sendo comparados sao iguais.
+     */
     public boolean equals(Object object) {
         if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
@@ -58,6 +133,11 @@ public class Pesquisador {
         return java.util.Objects.equals(email, that.email);
     }
 
+    /**
+     *  Gera um codigo HashCode para identificar o Pesquisador a partir de seu email.
+     *
+     * @return Inteiro representando o codigo gerado de um objeto Pesquisador.
+     */
     public int hashCode() {
         return java.util.Objects.hash(super.hashCode(), email);
     }
