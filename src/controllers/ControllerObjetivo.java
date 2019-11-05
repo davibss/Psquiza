cea81e5b60b04d1c273d0e6f32f8e87bd5704433
HenriqueGalindo <henrique.galindo@ccc.ufcg.edu.br>
2019-11-05 09:44:53 -0300

    Início da documentação do caso de uso 3

--- src/controllers/ControllerObjetivo.java
+++ src/controllers/ControllerObjetivo.java
@@ -7,15 +7,36 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
 
+/**
+ * Controller do objeto Objetivo
+ * @author HenriqueGalindo - 119110732
+ */
 public class ControllerObjetivo {
+    /** Mapa contendo os objetos Objetivo onde a chave é o código e o valor é o objetivo*/
     private Map<String, Objetivo> objetivos;
+    /** Contador que indica o id do próximo objetivo a ser cadastrado*/
     private int idObjetivo;
 
+    /**
+     * Constrói o controller e inicializa o mapa e o contador
+     * idObjetivo inicia como 1, pois este é o id do primeiro objetivo cadastrado
+     */
     public ControllerObjetivo() {
         this.objetivos = new HashMap<>();
         this.idObjetivo = 1;
     }
 
+    /**
+     * Cadastra objetivos no mapa de objetivos
+     * Verifica se os parâmetros são vazios ou nulos
+     * Verifica se tipo é GERAL ou ESPECIFICO
+     * Verifica se aderencia e viabilidade estão entre 1 e 5
+     * Gera o código do objetivo cadastrado
+     * @param tipo representação em String do tipo do objetivo [GERAL ou ESPECIFICO]
+     * @param descricao representação em String da decricao do objetivo
+     * @param aderencia valor inteiro entre 1 e 5 que representa a aderencia do objetivo
+     * @param viabilidade valor inteiro entre 1 e 5 que representa a viabilidade do objetivo
+     */
     public void cadastraObjetivo(String tipo, String descricao, int aderencia, int viabilidade) {
         verificaValidadeObjetivo(tipo, descricao, aderencia, viabilidade);
         String chave = "O"+(this.idObjetivo);
@@ -23,6 +44,14 @@ public class ControllerObjetivo {
         this.idObjetivo += 1;
     }
 
+    /**
+     * Verifica se os parametros são vazios ou nulos
+     * Verifica se tipo é GERAL ou ESPECIFICO e se aderencia e viabilidade estão entre 1 e 5
+     * @param tipo objeto String sob teste
+     * @param descricao objeto String sob teste
+     * @param aderencia int sob teste
+     * @param viabilidade int sob teste
+     */
     private void verificaValidadeObjetivo(String tipo, String descricao, int aderencia, int viabilidade) {
         verificaVazioNulo(descricao, "descricao");
         verificaVazioNulo(tipo, "tipo");
@@ -37,6 +66,12 @@ public class ControllerObjetivo {
         }
     }
 
+    /**
+     * Remove objetivos do mapa de objetivos
+     * Verifica se o parâmetro passado é vazio ou nulo
+     * Verifica se o mapa contém o objetivo que deve ser removido
+     * @param codigo representação em String do código do objetivo a ser removido
+     */
     public void apagarObjetivo(String codigo) {
         verificaVazioNulo(codigo, "codigo");
         if(!this.objetivos.containsKey(codigo)) {
@@ -45,6 +80,13 @@ public class ControllerObjetivo {
         this.objetivos.remove(codigo);
     }
 
+    /**
+     * Retorna a representação em String de um objeto Objetivo
+     * Verifica se o parâmetro passado é vazio ou nulo
+     * Verifica se o mapa contém o objetivo que deve ser exibido
+     * @param codigo representação em String do código do objetivo a ser exibido
+     * @return representação em String de um objeto Objetivo
+     */
     public String exibeObjetivo(String codigo) {
         verificaVazioNulo(codigo, "codigo");
         if(!this.objetivos.containsKey(codigo)) {
@@ -53,6 +95,12 @@ public class ControllerObjetivo {
         return codigo + " - " + this.objetivos.get(codigo).toString();
     }
 
+    /**
+     * Verifica se o parâmetro passado é vazio ou nulo, se for, monta String
+     * pra lançar uma exceção.
+     * @param atributo representação em String do atributo a ser verificado.
+     * @param nomeAtributo representação em String do nome do atributo.
+     */
     private void verificaVazioNulo(String atributo, String nomeAtributo) {
         StringJoiner joiner = new StringJoiner(" ");
 
--- src/controllers/ControllerProblema.java
+++ src/controllers/ControllerProblema.java
@@ -6,15 +6,33 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
 
+/**
+ * Controller do objeto Problema
+ * @author HenriqueGalindo - 119110732
+ */
 public class ControllerProblema {
+    /** Mapa contendo os objetos Problema onde a chave é o código e o valor é o problema*/
     private Map<String, Problema> problemas;
+    /** Contador que indica o id do próximo problema a ser cadastrado*/
     private int idProblema;
 
+    /**
+     * Constrói o controller e inicializa o mapa e o contador
+     * idProblema inicia como 1, pois este é o id do primeiro problema cadastrado
+     */
     public ControllerProblema() {
         this.problemas = new HashMap<>();
         this.idProblema = 1;
     }
 
+    /**
+     * Cadastra problemas no mapa de problemas
+     * Verifica se os parâmetros são vazios ou nulos
+     * Verifica se viabilidade está entre 1 e 5
+     * Gera o código do problema cadastrado
+     * @param descricao representação em String da decricao do problema
+     * @param viabilidade valor inteiro entre 1 e 5 que representa a viabilidade do problema
+     */
     public void cadastraProblema(String descricao, int viabilidade) {
         verificaValidadeProblema(descricao, viabilidade);
         String chave = "P"+(this.idProblema);
@@ -22,6 +40,11 @@ public class ControllerProblema {
         this.idProblema += 1;
     }
 
+    /**
+     * Verifica se os parametros são vazios ou nulos e se viabilidade está entre 1 e 5
+     * @param descricao objeto String sob teste
+     * @param viabilidade int sob teste
+     */
     private void verificaValidadeProblema(String descricao, int viabilidade) {
         verificaVazioNulo(descricao, "descricao");
         if(viabilidade < 1 || viabilidade > 5) {
@@ -29,6 +52,12 @@ public class ControllerProblema {
         }
     }
 
+    /**
+     * Remove problemas no mapa de problemas
+     * Verifica se o parâmetro passado é vazio ou nulo
+     * Verifica se o mapa contém o problema que deve ser removido
+     * @param codigo representação em String do código do problema a ser removido
+     */
     public void apagarProblema(String codigo) {
         verificaVazioNulo(codigo, "codigo");
         verificaProblema(codigo);
@@ -38,6 +67,13 @@ public class ControllerProblema {
         this.problemas.remove(codigo);
     }
 
+    /**
+     * Retorna a representação em String de um objeto Problema
+     * Verifica se o parâmetro passado é vazio ou nulo
+     * Verifica se o mapa contém o problema que deve ser exibido
+     * @param codigo representação em String do código do problema a ser exibido
+     * @return representação em String de um objeto Problema
+     */
     public String exibeProblema(String codigo) {
         verificaVazioNulo(codigo,"codigo");
         verificaProblema(codigo);
