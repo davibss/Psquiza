2b59edae8b8f152707c67211274a4dd3773ff0d3
davibss <davi.sousa@ccc.ufcg.edu.br>
2019-11-20 19:46:41 -0300

    - teste de unidade do caso de uso 11 completo

--- tests/unit-tests/PesquisaTest.java
+++ tests/unit-tests/PesquisaTest.java
@@ -63,6 +63,8 @@ class PesquisaTest {
     void associaObjetivo() {
         assertTrue(pesquisaTest.asssociaObjetivo(
                 new Objetivo("O1", "GERAL", "Melhoria no ensino de OO.", 2, 3)));
+        assertTrue(pesquisaTest.asssociaObjetivo(
+                new Objetivo("O2", "GERAL", "Melhoria no ensino de P2.", 2, 3)));
         assertFalse(pesquisaTest.asssociaObjetivo(
                 new Objetivo("O1", "GERAL", "Melhoria no ensino de OO.", 2, 3)));
     }
@@ -77,7 +79,7 @@ class PesquisaTest {
     @Test
     void getObjetivos() {
         associaObjetivo();
-        assertEquals(1, pesquisaTest.getObjetivos());
+        assertEquals(2, pesquisaTest.getObjetivos());
     }
 
     @Test
@@ -92,15 +94,22 @@ class PesquisaTest {
     void contemObjetivo() {
         associaObjetivo();
         assertTrue(pesquisaTest.contemObjetivo(new Objetivo("O1", "GERAL", "Melhoria no ensino de OO.", 2, 3)));
-        assertFalse(pesquisaTest.contemObjetivo(new Objetivo("O2", "GERAL", "Melhoria no ensino de OO.", 2, 3)));
+        assertTrue(pesquisaTest.contemObjetivo(new Objetivo("O2", "GERAL", "Melhoria no ensino de P2.", 2, 3)));
+        assertFalse(pesquisaTest.contemObjetivo(new Objetivo("O3", "GERAL", "Melhoria no ensino de computação.", 2, 3)));
     }
 
     @Test
     void associaAtividade() {
-        assertTrue(pesquisaTest.associaAtividade("A1",
-                new Atividade("A1", "Monitoramento facebook/messenger", "ALTO", "É muito grave.")));
-        assertFalse(pesquisaTest.associaAtividade("A1",
-                new Atividade("A1", "Monitoramento facebook/messenger", "ALTO", "É muito grave.")));
+        Atividade atividade = new Atividade("A1", "Monitoramento de chats dos alunos de computacao do primeiro periodo.", "ALTO", "É muito grave.");
+        atividade.cadastraItem("Monitoramento facebook/messenger");
+        atividade.cadastraItem("Monitoramento slack");
+        atividade.cadastraItem("Monitoramento discord");
+        atividade.cadastraItem("Monitoramento whatsapp");
+        atividade.cadastraResultado("Resultado 1");
+        atividade.cadastraResultado("Resultado 2");
+        atividade.executaAtividade(1, 5);
+        assertTrue(pesquisaTest.associaAtividade("A1", atividade));
+        assertFalse(pesquisaTest.associaAtividade("A1", atividade));
     }
 
     @Test
@@ -113,6 +122,19 @@ class PesquisaTest {
 
     @Test
     void proximaAtividade() {
+        associaAtividade();
+        Atividade atividade2 = new Atividade("A2", "Degustacao de uma nova remeca de cervejas, criadas a partir de um novo processo" +
+                " de fermentacao.", "MEDIO", "Degustadores podem sofrer com problemas de saude nessa atividade, tal como" +
+                " ser alergico a algum ingrediente da cerveja.");
+        atividade2.cadastraItem("Item 1");
+        atividade2.cadastraItem("Item 2");
+        atividade2.executaAtividade(1, 3);
+        pesquisaTest.associaAtividade("A2", atividade2);
+
+        assertEquals("A1", pesquisaTest.proximaAtividade("MAIS_ANTIGA"));
+        assertEquals("A2", pesquisaTest.proximaAtividade("MENOS_PENDENCIAS"));
+        assertEquals("A1", pesquisaTest.proximaAtividade("MAIOR_RISCO"));
+        assertEquals("A1", pesquisaTest.proximaAtividade("MAIOR_DURACAO"));
 
     }
 
@@ -148,17 +170,33 @@ class PesquisaTest {
 
     @Test
     void getProblemaResumo() {
+        associaProblema();
+        assertEquals("P1 - Um problema. - 2", pesquisaTest.getProblemaResumo());
     }
 
     @Test
     void getObjetivosResumo() {
+        associaObjetivo();
+        assertEquals("        - O1 - GERAL - Melhoria no ensino de OO. - 5\n" +
+                              "        - O2 - GERAL - Melhoria no ensino de P2. - 5", pesquisaTest.getObjetivosResumo());
     }
 
     @Test
     void getAtividadesResumo() {
+        associaAtividade();
+        assertEquals("        - Monitoramento de chats dos alunos de computacao do primeiro periodo. (ALTO - É muito grave.)\n" +
+                "            - REALIZADO - ITEM1\n" +
+                "            - PENDENTE - ITEM2\n" +
+                "            - PENDENTE - ITEM3\n" +
+                "            - PENDENTE - ITEM4", pesquisaTest.getAtividadesResumo());
     }
 
     @Test
     void getAtividadesResultado() {
+        associaAtividade();
+        assertEquals("        - Monitoramento de chats dos alunos de computacao do primeiro periodo.\n" +
+                "            - ITEM1 - 5\n" +
+                "            - Resultado 1\n" +
+                "            - Resultado 2", pesquisaTest.getAtividadesResultado());
     }
 }
