30f3167d4a8a8444ee5f2f7a68cb44bf41872ff1
davibss <davi.sousa@ccc.ufcg.edu.br>
2019-11-21 08:11:52 -0300

    - teste de unidade do caso de uso 12 completo

--- tests/unit-tests/Cdu12Parte1Test.java
+++ tests/unit-tests/Cdu12Parte1Test.java
@@ -32,33 +32,33 @@ class Cdu12Parte1Test {
     }
 
     private void cadastrarPesquisas() {
-        controllerPesquisa.cadastrarPesquisa("Identificacao de buracos negros com uso de programacao", "astronomia, computacao");
-        controllerPesquisa.cadastrarPesquisa("Alienacao Parental e o Sistema de Justica Brasileiro.", "psicologia, sistema juridico, alienacao parental, brasil");
+        controllerGeral.cadastrarPesquisa("Identificacao de buracos negros com uso de programacao", "astronomia, computacao");
+        controllerGeral.cadastrarPesquisa("Alienacao Parental e o Sistema de Justica Brasileiro.", "psicologia, sistema juridico, alienacao parental, brasil");
     }
 
     private void cadastrarPesquisadores() {
-        controllerPesquisador.cadastraPesquisador("killua zoldyck", "estudante", "Interessado em eletricidade, o terceiro de cinco filhos da famosa familia Zaoldyeck.",
+        controllerGeral.cadastraPesquisador("killua zoldyck", "estudante", "Interessado em eletricidade, o terceiro de cinco filhos da famosa familia Zaoldyeck.",
                 "hunterxhunter@1998","https://godspeed");
-        controllerPesquisador.cadastraPesquisador("heisenberg", "professor", "Interessado nos efeitos da metafetamina e no estudo sobre o cancer. Pesquisador principal da pesquisa de radigrafia a fotons, peca fundamental na pesquisa que ganhou um premio nobel.",
+        controllerGeral.cadastraPesquisador("heisenberg", "professor", "Interessado nos efeitos da metafetamina e no estudo sobre o cancer. Pesquisador principal da pesquisa de radigrafia a fotons, peca fundamental na pesquisa que ganhou um premio nobel.",
                 "breakingbad@2008","https://iamthedanger");
     }
 
     private void cadastrarObjetivos() {
-        controllerObjetivo.cadastraObjetivo("ESPECIFICO", "Obter a preferencia de filmes e series de um usuario.", 2, 3);
-        controllerObjetivo.cadastraObjetivo("GERAL", "Saber qual epoca do ano eh melhor para criacao de frutos do mar.", 1,1);
-        controllerObjetivo.cadastraObjetivo("ESPECIFICO", "Testar os conhecimentos dos alunos de P2 em OO.", 3,4);
+        controllerGeral.cadastraObjetivo("ESPECIFICO", "Obter a preferencia de filmes e series de um usuario.", 2, 3);
+        controllerGeral.cadastraObjetivo("GERAL", "Saber qual epoca do ano eh melhor para criacao de frutos do mar.", 1,1);
+        controllerGeral.cadastraObjetivo("ESPECIFICO", "Testar os conhecimentos dos alunos de P2 em OO.", 3,4);
     }
 
     private void cadastrarProblemas() {
-        controllerProblema.cadastraProblema("Dificuldade no aprendizado de OO no 2 periodo.", 2);
-        controllerProblema.cadastraProblema("Problema em manter area de conservacao das especies marinhas.", 1);
+        controllerGeral.cadastraProblema("Dificuldade no aprendizado de OO no 2 periodo.", 2);
+        controllerGeral.cadastraProblema("Problema em manter area de conservacao das especies marinhas.", 1);
     }
 
     private void cadastraAtividades() {
-        controllerAtividade.cadastrarAtividades("Realizar entrevistas com estudantes do primeiro periodo.", "BAIXO", "O risco de apenas realizar entrevistas não é elevado.");
-        controllerAtividade.cadastrarAtividades("Realizar o monitoramento de postes eletricos.", "ALTO", "Lidar com equipamentos eletricos de alta potencia pode levar a diversos acidentes.");
-        controllerAtividade.cadastraItem("A1", "Entrevistas com alunos de Ciencia da Computacao");
-        controllerAtividade.cadastraItem("A1", "Entrevistas com alunos de Engenharia Eletrica");
+        controllerGeral.cadastrarAtividade("Realizar entrevistas com estudantes do primeiro periodo.", "BAIXO", "O risco de apenas realizar entrevistas não é elevado.");
+        controllerGeral.cadastrarAtividade("Realizar o monitoramento de postes eletricos.", "ALTO", "Lidar com equipamentos eletricos de alta potencia pode levar a diversos acidentes.");
+        controllerGeral.cadastraItem("A1", "Entrevistas com alunos de Ciencia da Computacao");
+        controllerGeral.cadastraItem("A1", "Entrevistas com alunos de Engenharia Eletrica");
     }
     @Test
     void salva() {
--- tests/unit-tests/Cdu12Parte2Test.java
+++ tests/unit-tests/Cdu12Parte2Test.java
@@ -28,40 +28,40 @@ class Cdu12Parte2Test {
 
     @Test
     void testaPesquisas() {
-        assertEquals("AST1 - Identificacao de buracos negros com uso de programacao - astronomia, computacao", controllerPesquisa.exibirPesquisa("AST1"));
-        assertEquals("PSI1 - Alienacao Parental e o Sistema de Justica Brasileiro. - psicologia, sistema juridico, alienacao parental, brasil", controllerPesquisa.exibirPesquisa("PSI1"));
+        assertEquals("AST1 - Identificacao de buracos negros com uso de programacao - astronomia, computacao", controllerGeral.exibirPesquisa("AST1"));
+        assertEquals("PSI1 - Alienacao Parental e o Sistema de Justica Brasileiro. - psicologia, sistema juridico, alienacao parental, brasil", controllerGeral.exibirPesquisa("PSI1"));
     }
 
     @Test
     void testaPesquisadores() {
-        assertEquals("killua zoldyck (estudante) - Interessado em eletricidade, o terceiro de cinco filhos da famosa familia Zaoldyeck. - hunterxhunter@1998 - https://godspeed", controllerPesquisador.exibePesquisador("hunterxhunter@1998"));
+        assertEquals("killua zoldyck (estudante) - Interessado em eletricidade, o terceiro de cinco filhos da famosa familia Zaoldyeck. - hunterxhunter@1998 - https://godspeed", controllerGeral.exibePesquisador("hunterxhunter@1998"));
         assertEquals("heisenberg (professor) - Interessado nos efeitos da metafetamina e no estudo sobre o cancer. Pesquisador principal da pesquisa de radigrafia a fotons, peca fundamental na pesquisa que ganhou um premio nobel." +
-                " - breakingbad@2008 - https://iamthedanger", controllerPesquisador.exibePesquisador("breakingbad@2008"));
+                " - breakingbad@2008 - https://iamthedanger", controllerGeral.exibePesquisador("breakingbad@2008"));
     }
 
     @Test
     void testaObjetivos() {
-        assertEquals("01 - ESPECIFICO - Obter a preferencia de filmes e series de um usuario. - 5",
-                controllerObjetivo.exibeObjetivo("01"));
-        assertEquals("02 - GERAL - Saber qual epoca do ano eh melhor para criacao de frutos do mar. - 2",
-                controllerObjetivo.exibeObjetivo("02"));
-        assertEquals("03 - ESPECIFICO - Testar os conhecimentos dos alunos de P2 em OO. - 7",
-                controllerObjetivo.exibeObjetivo("03"));
+        assertEquals("O1 - ESPECIFICO - Obter a preferencia de filmes e series de um usuario. - 5",
+                controllerGeral.exibeObjetivo("O1"));
+        assertEquals("O2 - GERAL - Saber qual epoca do ano eh melhor para criacao de frutos do mar. - 2",
+                controllerGeral.exibeObjetivo("O2"));
+        assertEquals("O3 - ESPECIFICO - Testar os conhecimentos dos alunos de P2 em OO. - 7",
+                controllerGeral.exibeObjetivo("O3"));
     }
 
     @Test
     void testaProblemas() {
         assertEquals("P1 - Dificuldade no aprendizado de OO no 2 periodo. - 2",
-                controllerProblema.exibeProblema("P1"));
+                controllerGeral.exibeProblema("P1"));
         assertEquals("P2 - Problema em manter area de conservacao das especies marinhas. - 1",
-                controllerProblema.exibeProblema("P2"));
+                controllerGeral.exibeProblema("P2"));
     }
 
     @Test
     void testaAtividades() {
         assertEquals("Realizar entrevistas com estudantes do primeiro periodo. (BAIXO - O risco de apenas realizar entrevistas não é elevado.) | PENDENTE - Entrevistas com alunos de Ciencia da Computacao" +
-                " | PENDENTE - Entrevistas com alunos de Engenharia Eletrica", controllerAtividade.exibeAtividade("A1"));
-        assertEquals("Realizar o monitoramento de postes eletricos. (ALTO - Lidar com equipamentos eletricos de alta potencia pode levar a diversos acidentes.)", controllerAtividade.exibeAtividade("A2"));
+                " | PENDENTE - Entrevistas com alunos de Engenharia Eletrica", controllerGeral.exibeAtividade("A1"));
+        assertEquals("Realizar o monitoramento de postes eletricos. (ALTO - Lidar com equipamentos eletricos de alta potencia pode levar a diversos acidentes.)", controllerGeral.exibeAtividade("A2"));
     }
 
 }
